af5b0bb63c5d95fab518a29487476efbc1282d3a
Sam Bulatov <mephistorine@gmail.com>
2022-06-21 23:00:00 +0300

    feat: MDX парсинг + отображение

--- apps/mephi.dev/next.config.js
+++ apps/mephi.dev/next.config.js
@@ -1,15 +1,25 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withNx = require("@nrwl/next/plugins/with-nx");
 
+const withMDX = require("@next/mdx")({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+    providerImportSource: "@mdx-js/react"
+  }
+});
+
 /**
  * @type {import("@nrwl/next/plugins/with-nx").WithNxOptions}
  **/
-const nextConfig = {
+const nextConfig = withMDX({
   nx: {
     // Set this to true if you would like to to use SVGR
     // See: https://github.com/gregberge/svgr
     svgr: false
-  }
-};
+  },
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx']
+})
 
 module.exports = withNx(nextConfig);
--- apps/mephi.dev/pages/articles/[slug].tsx
+++ apps/mephi.dev/pages/articles/[slug].tsx
@@ -1,3 +1,54 @@
-export function Article() {
+import { readdirSync, readFileSync } from "fs"
+import matter from "gray-matter"
+import { GetStaticPaths, GetStaticProps } from "next"
+import { MDXRemote, MDXRemoteSerializeResult } from "next-mdx-remote"
+import { serialize } from "next-mdx-remote/serialize"
+import { join } from "path"
 
+const articlesPath = join(process.cwd(), "apps", "mephi.dev", "_articles")
+
+interface ArticleProps {
+  source: MDXRemoteSerializeResult<Record<string, unknown>>
+  metadata: Record<string, any>
+}
+
+export const getStaticPaths: GetStaticPaths = () => {
+  const paths: { params: { slug: string } }[] = readdirSync(articlesPath)
+    .filter((path) => /\.mdx?$/.test(path))
+    .map((path) => path.replace(/\.mdx?$/, ''))
+    .map((slug) => ({ params: { slug } }))
+
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export const getStaticProps: GetStaticProps<ArticleProps, { slug: string }> = async ({ params }) => {
+  const articleFilePath: string = join(articlesPath, `${ params.slug }.mdx`)
+  const source: Buffer = readFileSync(articleFilePath)
+  const frontMatterResult: matter.GrayMatterFile<Buffer> = matter(source)
+
+  const mdxSerializeResult: MDXRemoteSerializeResult<Record<string, unknown>> = await serialize(frontMatterResult.content, {
+    mdxOptions: {
+      remarkPlugins: [],
+      rehypePlugins: [],
+    },
+    scope: frontMatterResult.data,
+  })
+
+  return {
+    props: {
+      source: mdxSerializeResult,
+      metadata: frontMatterResult.data
+    }
+  }
+}
+
+export default function Article({ source }) {
+  return (
+    <div>
+      <MDXRemote {...source} />
+    </div>
+  )
 }
